7ab359f89f358f98a1a4536349c259c107935fd5
Gustavo Muniz Martins <gustamunizmartins@gmail.com>
2022-06-28 11:56:47 -0300

    adding fonts and favicon

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -29,13 +29,22 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     @media (max-width: 720px) {
-      font-size: 87.50%;
+      font-size: 87.50%; // 14px
     }
   }
 
   body {
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
